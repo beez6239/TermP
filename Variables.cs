25a236255cd6e29b2d28876ebe8a58d9a30459c1
OSAS <OSAS@DESKTOP-GE8PFTU>
2022-11-10 20:29:51 +0000

    Add project files.

---
+++ Variables.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class Variables
+    {
+        public string FullName { get; set; }
+        public string Phone { get; set; }
+        public string ID_NO { get; set; }
+        public string EmailID { get; set; }
+    }
+}
